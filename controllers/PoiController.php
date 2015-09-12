58fb6fedb4fbeb999faaf735ac95cad1da5d96d4
Christoph Bühler <christoph.buehler@hotmail.com>
2015-09-12 19:16:03 +0200

    Native php functions backslash because of namespace.

--- controllers/PoiController.php
+++ controllers/PoiController.php
@@ -12,23 +12,23 @@ class PoiController extends BaseController
     public $modelClass = 'app\models\Poi';
 
     public static function resizeImage($imgString, $width, $height) {
-      $img = imagecreatefromstring($imgString);
+      $img = \imagecreatefromstring($imgString);
 
     	// Ensure image is in jpg format
     	if ($img["type"] != "image/jpeg" && $img["type"] != "image/jpg") return false;
 
-    	$tmp = imagecreatetruecolor($width, $height);
+    	$tmp = \imagecreatetruecolor($width, $height);
 
-    	$initSize = getimagesize($img["tmp_name"]);
+    	$initSize = \getimagesize($img["tmp_name"]);
 
-    	imagecopyresampled($tmp, imagecreatefromjpeg($img["tmp_name"]), 0, 0, 0, 0, $width, $height, $initSize[0], $initSize[1]);
+    	\imagecopyresampled($tmp, \imagecreatefromjpeg($img["tmp_name"]), 0, 0, 0, 0, $width, $height, $initSize[0], $initSize[1]);
 
-    	ob_start();
-    	imagejpeg($tmp, NULL, 100);
-    	imagedestroy($tmp);
-    	$thumbnail = ob_get_clean();
+    	\ob_start();
+    	\imagejpeg($tmp, NULL, 100);
+    	\imagedestroy($tmp);
+    	$thumbnail = \ob_get_clean();
 
-    	return  base64_encode($thumbnail);
+    	return \base64_encode($thumbnail);
     }
 
 }
