50f3d4afcd951f924c30d13eb08d2637c2ee0659
Bolaji Smith <bmaq007@gmail.com>
2015-09-12 14:15:50 +0200

    Exposed Comments endpoint

---
+++ controllers/CommentController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\controllers;
+
+/**
+ * Description of COmmentController
+ *
+ * @author Bolaji Smith
+ */
+class CommentController extends BaseController
+{
+    public $modelClass = 'app\models\Comment';
+    
+    
+}
+
