9a40eb13b93355bf4a07bfcf3e89690bf8992a22
Bolaji Smith <bmaq007@gmail.com>
2015-09-12 13:35:03 +0200

    Setup for remote and local databases

--- config/db.php
+++ config/db.php
@@ -1,9 +1,19 @@
 <?php
 
-return [
+$debug =  [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=hackathon',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
 ];
+
+$live = [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=hackathon',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
+
+return YII_DEBUG ? $debug : $live;
