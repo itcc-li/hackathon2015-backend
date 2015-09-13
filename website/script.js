d1dadcba891f3e70700d677282bcfe119ca2d914
Christoph Bühler <christoph.buehler@hotmail.com>
2015-09-13 13:20:43 +0200

    debugging

--- website/script.js
+++ website/script.js
@@ -1,6 +1,4 @@
 $(function() {
-	var map = $('#map')[0];
-	
 	$.get('http://52.24.125.211/api/index.php/pois', function(data) {
 		data.map(function(location) {
 			var marker = new google.maps.Marker({
@@ -8,6 +6,7 @@ $(function() {
 				map: map,
 				title: 'Gaflei'
 			});
+			console.log(location);
 		});
 		console.log(data);
 	});
