b66cc77e232b9a195e0c1b0e4dde2745b3feea87
Christoph Bühler <christoph.buehler@hotmail.com>
2015-09-13 15:50:27 +0200

    check hash

--- website/script.js
+++ website/script.js
@@ -1,15 +1,26 @@
 $(function() {
-	var hash = location.hash;
-	if (hash === '') return;
-	hash = hash.substr(1);
-	$.post('content/' + hash + '.html', function(data) {
-		showContent(data);
-	});
-	$('#close-page-content-btn').click(function() {
-		$('#page-content').fadeOut();
-	});
+
+	checkHash();
+	
 	function showContent(html) {
 		$('#content').html(html);
 		$('#page-content').fadeIn();
 	}
+	
+	function checkHash() {
+		var hash = location.hash;
+		if (hash === '') return;
+		
+		hash = hash.substr(1);
+		$.post('content/' + hash + '.html', function(data) {
+			showContent(data);
+		});
+		$('#close-page-content-btn').click(function() {
+			$('#page-content').fadeOut();
+		});
+	}
+	
+	$(window).hashchange( function() {
+		checkHash();
+	});
 });
