0cdde987ae3e4bf4d18ee39b409ae72fc9d6a8e3
vaziuok <vaziuok@users.noreply.github.com>
2015-09-13 15:06:38 +0200

    Center maps with bounds

--- website/map.js
+++ website/map.js
@@ -1,69 +1,73 @@
 var map;
-$(function() {
-		google.maps.event.addDomListener(window, 'load', function() {
-			initialize();
+var bounds = new google.maps.LatLngBounds();
 
-			$.get('http://52.24.125.211/api/index.php/pois', function(data) {
-			data.map(function(location) {
-				var pos = { lat: Number(location.latitude), lng: Number(location.longitude) };
-				new google.maps.Marker({
-					position: pos,
-					map: map,
-					icon: 'images/marker.png',
-					title: location.name
-				});
-			});
-		});
-	});
+$(function() {
+        google.maps.event.addDomListener(window, 'load', function() {
+            initialize();
+
+            $.get('http://52.24.125.211/api/index.php/pois', function(data) {
+            data.map(function(location) {
+                var pos = { lat: Number(location.latitude), lng: Number(location.longitude) };
+                new google.maps.Marker({
+                    position: pos,
+                    map: map,
+                    icon: 'images/marker.png',
+                    title: location.name
+                });
+                bounds.extend(new google.maps.LatLng(location.latitude, location.longitude));
+            });
+        });
+    });
 });
 
 function initialize() {
-	var mapCanvas = document.getElementById('map');
+    var mapCanvas = document.getElementById('map');
 
-	var styles = [
-	  {
-		stylers: [
-		  { hue: "#00ffe6" },
-		  { saturation: -20 }
-		]
-	  }, {
-		featureType: "road",
-		elementType: "geometry",
-		stylers: [
-		  { lightness: 100 },
-		  { visibility: "simplified" }
-		]
-	  }, {
-		featureType: "road",
-		elementType: "labels",
-		stylers: [
-		  { visibility: "off" }
-		]
-	  }
-	];
+    var styles = [
+      {
+        stylers: [
+          { hue: "#00ffe6" },
+          { saturation: -20 }
+        ]
+      }, {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [
+          { lightness: 100 },
+          { visibility: "simplified" }
+        ]
+      }, {
+        featureType: "road",
+        elementType: "labels",
+        stylers: [
+          { visibility: "off" }
+        ]
+      }
+    ];
 
-	// Default coordinates, if geolocation doesn't works
-	var coordinates = {
-		latitude: 47.139495,
-		longitude: 9.524542
-	};
+    // Default coordinates, if geolocation doesn't works
+    var coordinates = {
+        latitude: 47.139495,
+        longitude: 9.524542
+    };
 
     var mapOptions = {
         center: new google.maps.LatLng(coordinates.latitude, coordinates.longitude),
         zoom: 14,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
-		styles: styles,
-		disableDefaultUI: true,
-		draggable: false,
-		scrollwheel: false,
-		disableDoubleClickZoom: true
+        styles: styles,
+        disableDefaultUI: true,
+        draggable: false,
+        scrollwheel: false,
+        disableDoubleClickZoom: true
     }
 
     map = new google.maps.Map(mapCanvas, mapOptions);
+    map.fitBounds(bounds);
 
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(function (position) {
-			map.setCenter(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            map.setCenter(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
         });
-	}
+    }
 }
