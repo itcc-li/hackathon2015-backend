289a9da0debfe444bf40b5cfe84121ad5eba0b96
Christoph Bühler <christoph.buehler@hotmail.com>
2015-09-13 15:20:46 +0200

    hash nav load template test

--- website/script.js
+++ website/script.js
@@ -1,3 +1,11 @@
 $(function() {
-	
+	var hash = location.hash;
+	if (hash === '') return;
+	hash = hash.substr(1);
+	$.post(hash + '.html', function(data) {
+		showContent(data);
+	});
+	function showContent(html) {
+		console.log(html);
+	}
 });
